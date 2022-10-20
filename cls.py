67955bebc8d3fef1e5ab036f0effe37ac6aa56c0
VIVEK SAHU <mrl9viveksahu@gmail.com>
2022-10-20 15:55:47 +0530

    Two new python files added
    Two new python files added

---
+++ cls.py
@@ -0,0 +1,11 @@
+class railwayf:
+    type = "Railway"
+    def data(self):
+        print(self.name)
+        print(self.train)
+
+appl = railwayf()
+appl.name = "vivek"
+appl.train = "rajdhani"
+appl.data()
+
---
+++ condition.py
@@ -0,0 +1,7 @@
+a = int(input("Enter Number :"))
+if(a>0):
+    print("Number is positive")
+elif(a<0):
+    print("Number is negative") 
+else:
+    print("number is zero")
